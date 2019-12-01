dc1f668540dba810767f1c743bd5df6044ad36b3
yashssonani <49947849+yashssonani@users.noreply.github.com>
2019-12-01 18:06:15 +0530

    Add files via upload

--- uniborg/uniborg.py
+++ uniborg/uniborg.py
@@ -3,7 +3,6 @@
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 import asyncio
 import importlib.util
-import inspect
 import logging
 from pathlib import Path
 
@@ -11,27 +10,38 @@ from telethon import TelegramClient
 import telethon.utils
 import telethon.events
 
-from .storage import Storage
 from . import hacks
 
 
 class Uniborg(TelegramClient):
     def __init__(
-            self, session, *, plugin_path="plugins", storage=None,
-            bot_token=None, **kwargs):
-        # TODO: handle non-string session
-        #
-        # storage should be a callable accepting plugin name -> Storage object.
-        # This means that using the Storage type as a storage would work too.
-        self._name = session
-        self.storage = storage or (lambda n: Storage(Path("data") / n))
-        self._logger = logging.getLogger(session)
+            self, session, *, n_plugin_path="plugins", db_plugin_path="plugins",
+            bot_token=None, api_config=None, **kwargs):
+        self._name = "LoggedIn"
+        self._logger = logging.getLogger("UniBorg")
         self._plugins = {}
-        self._plugin_path = plugin_path
+        self.n_plugin_path = n_plugin_path
+        self.db_plugin_path = db_plugin_path
+        self.config = api_config
 
         kwargs = {
-            "api_id": 6, "api_hash": "eb06d4abfb49dc3eeb1aeb98ae0f581e",
-            **kwargs}
+            "api_id": 6,
+            "api_hash": "eb06d4abfb49dc3eeb1aeb98ae0f581e",
+            "device_model": "GNU/Linux nonUI",
+            "app_version": "@UniBorg 2.0",
+            "lang_code": "ml",
+            **kwargs
+        }
+
+        self.tgbot = None
+        if api_config.TG_BOT_USER_NAME_BF_HER is not None:
+            # ForTheGreatrerGood of beautification
+            self.tgbot = TelegramClient(
+                "TG_BOT_TOKEN",
+                api_id=api_config.APP_ID,
+                api_hash=api_config.API_HASH
+            ).start(bot_token=api_config.TG_BOT_TOKEN_BF_HER)
+
         super().__init__(session, **kwargs)
 
         # This is a hack, please avert your eyes
@@ -44,8 +54,28 @@ class Uniborg(TelegramClient):
         core_plugin = Path(__file__).parent / "_core.py"
         self.load_plugin_from_file(core_plugin)
 
-        for p in Path().glob(f"{self._plugin_path}/*.py"):
-            self.load_plugin_from_file(p)
+        inline_bot_plugin = Path(__file__).parent / "_inline_bot.py"
+        self.load_plugin_from_file(inline_bot_plugin)
+
+        for a_plugin_path in Path().glob(f"{self.n_plugin_path}/*.py"):
+            self.load_plugin_from_file(a_plugin_path)
+
+        if api_config.DB_URI is not None:
+            for a_plugin_path in Path().glob(f"{self.db_plugin_path}/*.py"):
+                self.load_plugin_from_file(a_plugin_path)
+
+        LOAD = self.config.LOAD
+        NO_LOAD = self.config.NO_LOAD
+        if LOAD or NO_LOAD:
+            to_load = LOAD
+            if to_load:
+                self._logger.info("Modules to LOAD: ")
+                self._logger.info(to_load)
+            if NO_LOAD:
+                for plugin_name in NO_LOAD:
+                    if plugin_name in self._plugins:
+                        self.remove_plugin(plugin_name)
+
 
     async def _async_init(self, **kwargs):
         await self.start(**kwargs)
@@ -53,8 +83,11 @@ class Uniborg(TelegramClient):
         self.me = await self.get_me()
         self.uid = telethon.utils.get_peer_id(self.me)
 
+        self._logger.info(f"Logged in as {self.uid}")
+
+
     def load_plugin(self, shortname):
-        self.load_plugin_from_file(f"{self._plugin_path}/{shortname}.py")
+        self.load_plugin_from_file(f"{self.n_plugin_path}/{shortname}.py")
 
     def load_plugin_from_file(self, path):
         path = Path(path)
@@ -66,13 +99,17 @@ class Uniborg(TelegramClient):
 
         mod.borg = self
         mod.logger = logging.getLogger(shortname)
-        mod.storage = self.storage(f"{self._name}/{shortname}")
+        # declare Config and tgbot to be accessible by all modules
+        mod.Config = self.config
+        if self.config.TG_BOT_USER_NAME_BF_HER is not None:
+            mod.tgbot = self.tgbot
+
 
         spec.loader.exec_module(mod)
         self._plugins[shortname] = mod
         self._logger.info(f"Successfully loaded plugin {shortname}")
 
-    async def remove_plugin(self, shortname):
+    def remove_plugin(self, shortname):
         name = self._plugins[shortname].__name__
 
         for i in reversed(range(len(self._event_builders))):
@@ -80,16 +117,7 @@ class Uniborg(TelegramClient):
             if cb.__module__ == name:
                 del self._event_builders[i]
 
-        plugin = self._plugins.pop(shortname)
-        if callable(getattr(plugin, 'unload', None)):
-            try:
-                unload = plugin.unload()
-                if inspect.isawaitable(unload):
-                    await unload
-            except Exception:
-                self._logger.exception(f'Unhandled exception unloading {shortname}')
-
-        del plugin
+        del self._plugins[shortname]
         self._logger.info(f"Removed plugin {shortname}")
 
     def await_event(self, event_matcher, filter=None):
--- uniborg/util.py
+++ uniborg/util.py
@@ -2,16 +2,63 @@
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
-import functools
 import re
-import signal
+import math
+import os
+import time
 
 from telethon import events
 from telethon.tl.functions.messages import GetPeerDialogsRequest
 
+# the secret configuration specific things
+ENV = bool(os.environ.get("ENV", False))
+if ENV:
+    from sample_config import Config
+else:
+    if os.path.exists("config.py"):
+        from config import Development as Config
 
-def admin_cmd(pattern):
-    return events.NewMessage(outgoing=True, pattern=re.compile(pattern))
+
+def admin_cmd(**args):
+    pattern = args.get("pattern", None)
+    allow_sudo = args.get("allow_sudo", False)
+
+    # get the pattern from the decorator
+    if pattern is not None:
+        if pattern.startswith("\#"):
+            # special fix for snip.py
+            args["pattern"] = re.compile(pattern)
+        else:
+            args["pattern"] = re.compile(Config.COMMAND_HAND_LER + pattern)
+
+    args["outgoing"] = True
+    # should this command be available for other users?
+    if allow_sudo:
+        args["from_users"] = list(Config.SUDO_USERS)
+        # Mutually exclusive with outgoing (can only set one of either).
+        args["incoming"] = True
+        del args["allow_sudo"]
+
+    # error handling condition check
+    elif "incoming" in args and not args["incoming"]:
+        args["outgoing"] = True
+
+    # add blacklist chats, UB should not respond in these chats
+    args["blacklist_chats"] = True
+    black_list_chats = list(Config.UB_BLACK_LIST_CHAT)
+    if len(black_list_chats) > 0:
+        args["chats"] = black_list_chats
+
+    # check if the plugin should allow edited updates
+    allow_edited_updates = False
+    if "allow_edited_updates" in args and args["allow_edited_updates"]:
+        allow_edited_updates = args["allow_edited_updates"]
+        del args["allow_edited_updates"]
+
+    # check if the plugin should listen for outgoing 'messages'
+    is_message_enabled = True
+
+    return events.NewMessage(**args)
 
 
 async def is_read(borg, entity, message, is_out=None):
@@ -31,19 +78,66 @@ async def is_read(borg, entity, message, is_out=None):
     max_id = dialog.read_outbox_max_id if is_out else dialog.read_inbox_max_id
     return message_id <= max_id
 
-def _handle_timeout(signum, frame):
-    raise TimeoutError("Execution took too long")
 
-def sync_timeout(seconds):
-    def decorator(func):
-        @functools.wraps(func)
-        def wrapper(*args, **kwargs):
-            signal.signal(signal.SIGALRM, _handle_timeout)
-            signal.setitimer(signal.ITIMER_REAL, seconds)
-            try:
-                r = func(*args, **kwargs)
-            finally:
-                signal.alarm(0)
-            return r
-        return wrapper
-    return decorator
+async def progress(current, total, event, start, type_of_ps):
+    """Generic progress_callback for both
+    upload.py and download.py"""
+    now = time.time()
+    diff = now - start
+    if round(diff % 10.00) == 0 or current == total:
+        percentage = current * 100 / total
+        speed = current / diff
+        elapsed_time = round(diff) * 1000
+        time_to_completion = round((total - current) / speed) * 1000
+        estimated_total_time = elapsed_time + time_to_completion
+        progress_str = "[{0}{1}]\nPercent: {2}%\n".format(
+            ''.join(["█" for i in range(math.floor(percentage / 5))]),
+            ''.join(["░" for i in range(20 - math.floor(percentage / 5))]),
+            round(percentage, 2))
+        tmp = progress_str + \
+            "{0} of {1}\nETA: {2}".format(
+                humanbytes(current),
+                humanbytes(total),
+                time_formatter(estimated_total_time)
+            )
+        await event.edit("{}\n {}".format(
+            type_of_ps,
+            tmp
+        ))
+
+
+def humanbytes(size):
+    """Input size in bytes,
+    outputs in a human readable format"""
+    # https://stackoverflow.com/a/49361727/4723940
+    if not size:
+        return ""
+    # 2 ** 10 = 1024
+    power = 2 ** 10
+    raised_to_pow = 0
+    dict_power_n = {
+        0: "",
+        1: "Ki",
+        2: "Mi",
+        3: "Gi",
+        4: "Ti"
+    }
+    while size > power:
+        size /= power
+        raised_to_pow += 1
+    return str(round(size, 2)) + " " + dict_power_n[raised_to_pow] + "B"
+
+
+def time_formatter(milliseconds: int) -> str:
+    """Inputs time in milliseconds, to get beautified time,
+    as string"""
+    seconds, milliseconds = divmod(int(milliseconds), 1000)
+    minutes, seconds = divmod(seconds, 60)
+    hours, minutes = divmod(minutes, 60)
+    days, hours = divmod(hours, 24)
+    tmp = ((str(days) + "d, ") if days else "") + \
+        ((str(hours) + "h, ") if hours else "") + \
+        ((str(minutes) + "m, ") if minutes else "") + \
+        ((str(seconds) + "s, ") if seconds else "") + \
+        ((str(milliseconds) + "ms, ") if milliseconds else "")
+    return tmp[:-2]
